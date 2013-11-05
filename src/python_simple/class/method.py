931e40342a635b3e5d8bba11b3829eddb3539e94
lili <lili00681@gmail.com>
2013-11-05 06:43:02 -0800

    add some python study

---
+++ src/python_simple/class/method.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+ #
+ # Creat Time :Tue 05 Nov 2013 06:10:28 AM GMT
+ # Autoor     : lili
+
+class My_first_method:
+# __init__ method can initaln
+	def __init__(self, name1, name2):
+		self.name1 = name1
+		self.name2 = name2
+	def say(self):
+		print '%s love %s.' % (self.name1, self.name2)
+
+p = My_first_method('lili', 'cici')
+p.say()
